e231ea19cde023e9b97066871700522088e2103b
Tung Nguyen <tung@vn2rap.com>
2020-07-22 02:23:56 +0700

    feat: stage #3 - make it your own
    - create the defined amount of cards
    - test the user's knowledge

--- Flashcards/task/src/flashcards/Main.kt
+++ Flashcards/task/src/flashcards/Main.kt
@@ -1,9 +1,25 @@
 package flashcards
 
 fun main() {
-    val term = readLine()!!.trim()
-    val definition = readLine()!!.trim()
-    val answer = readLine()!!.trim()
-    val result = if (answer == definition) "right!" else "wrong..."
-    println("Your answer is $result")
+    println("Input the number of cards:")
+    val numCards = readLine()!!.toInt()
+    // Create the defined amount of cards
+    val terms = Array(numCards) { "" }
+    val definitions = Array(numCards) { "" }
+    repeat(numCards) { it: Int ->
+        val cardNo = it + 1
+        println("The card #$cardNo:")
+        terms[it] = readLine()!!.trim()
+        println("The definition of the card #$cardNo:")
+        definitions[it] = readLine()!!.trim()
+    }
+    // Test the user's knowledge
+    repeat(numCards) { it: Int ->
+        println("Print the definition of \"${terms[it]}\"")
+        val answer = readLine()!!.trim()
+        println(when (answer) {
+            definitions[it] -> "Correct answer."
+            else -> "Wrong answer. The correct one is \"${definitions[it]}\""
+        })
+    }
 }
