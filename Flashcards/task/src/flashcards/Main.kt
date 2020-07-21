e9343ca12338ac7102e370025ec0fc886cef5855
Tung Nguyen <tung@vn2rap.com>
2020-07-22 01:15:37 +0700

    feat: stage #2 - what's on the card
    - create card from user input
    - print compare result

--- Flashcards/task/src/flashcards/Main.kt
+++ Flashcards/task/src/flashcards/Main.kt
@@ -1,12 +1,9 @@
 package flashcards
 
 fun main() {
-    println("Card:")
-    println("Kotlin") // term on the front of the card
-    println("Definition:")
-    println("Kotlin is a cross-platform, statically typed, general-purpose programming language with type inference." +
-            " Kotlin is designed to interoperate fully with Java, and the JVM version of its standard library depends" +
-            " on the Java Class Library, but type inference allows its syntax to be more concise. Kotlin mainly targets" +
-            " the JVM, but also compiles to JavaScript or native code (via LLVM). Language development costs are borne by" +
-            " JetBrains, while the Kotlin Foundation protects the Kotlin trademark.")
+    val term = readLine()!!.trim()
+    val definition = readLine()!!.trim()
+    val answer = readLine()!!.trim()
+    val result = if (answer == definition) "right!" else "wrong..."
+    println("Your answer is $result")
 }
