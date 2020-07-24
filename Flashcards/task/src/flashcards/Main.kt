0245976e894029a60ebbafd157820d568e06abda
Tung Nguyen <tung@vn2rap.com>
2020-07-25 03:53:07 +0700

    feat: stage #5 - menu, please!
    Implement actions:
    - add
    - remove
    - export
    - import
    - ask
    - exit

--- Flashcards/task/src/flashcards/Main.kt
+++ Flashcards/task/src/flashcards/Main.kt
@@ -1,40 +1,88 @@
 package flashcards
 
+import java.io.File
+import kotlin.random.Random
+
 fun main() {
-    println("Input the number of cards:")
-    val numCards = readLine()!!.toInt()
-    // Create the defined amount of cards
     val flashcards = mutableMapOf<String, String>()
-    repeat(numCards) { it: Int ->
-        val cardNo = it + 1
-        println("The card #$cardNo:")
-        var term = readLine()!!.trim()
-        while (flashcards.containsKey(term)) {
-            println("The card \"$term\" already exists. Try again:")
-            term = readLine()!!.trim()
-        }
-        println("The definition of the card #$cardNo:")
-        var definition = readLine()!!.trim()
-        while (flashcards.containsValue(definition)) {
-            println("The definition \"$definition\" already exists. Try again:")
-            definition = readLine()!!.trim()
-        }
-        flashcards[term] = definition
-    }
-    // Test the user's knowledge
-    for ((term, definition) in flashcards) {
-        println("Print the definition of \"$term\":")
-        val answer = readLine()!!.trim()
-        println(when {
-            answer == definition -> "Correct answer."
-            flashcards.containsValue(answer) -> {
-                val anotherTerm = flashcards.filter { it.value == answer }.keys.first()
-                getWrongMessage(definition, anotherTerm)
+    loop@ while (true) {
+        println("\nInput the action (add, remove, import, export, ask, exit):")
+        when (readInput()) {
+            "add" -> {
+                println("The card:")
+                val term = readInput()
+                if (flashcards.containsKey(term)) {
+                    println("The card \"$term\" already exists.")
+                    continue@loop
+                }
+                println("The definition of the card:")
+                val definition = readInput()
+                if (flashcards.containsValue(definition)) {
+                    println("The definition \"$definition\" already exists.")
+                    continue@loop
+                }
+                flashcards[term] = definition
+                println("The pair (\"$term\":\"$definition\") has been added.")
             }
-            else -> getWrongMessage(definition)
-        })
+            "remove" -> {
+                println("The card:")
+                val term = readInput()
+                if (!flashcards.containsKey(term)) {
+                    println("Can't remove \"$term\": there is no such card.")
+                    continue@loop
+                }
+                flashcards.remove(term)
+                println("The card has been removed.")
+            }
+            "export" -> {
+                println("File name:")
+                val file = File(readInput())
+                var content = ""
+                flashcards.forEach {
+                    val (term, definition) = it
+                    content += "$term=$definition\n"
+                }
+                file.writeText(content)
+                println("${flashcards.size} cards have been saved.")
+            }
+            "import" -> {
+                println("File name:")
+                val file = File(readInput())
+                if (!file.exists()) {
+                    println("File not found.")
+                    continue@loop
+                }
+                val cardList = file.readLines()
+                cardList.forEach {
+                    val (term, definition) = it.split('=')
+                    flashcards[term] = definition
+                }
+                println("${cardList.size} cards have been loaded.")
+            }
+            "ask" -> {
+                println("How many times to ask?")
+                val times = readLine()!!.toInt()
+                repeat(times) {
+                    val index = Random.nextInt(0, until = flashcards.size)
+                    val (term, definition) = flashcards.entries.elementAt(index)
+                    println("Print the definition of \"$term\":")
+                    val answer = readInput()
+                    println(when {
+                        answer == definition -> "Correct answer."
+                        flashcards.containsValue(answer) -> {
+                            val anotherTerm = flashcards.filter { it.value == answer }.keys.first()
+                            getWrongMessage(definition, anotherTerm)
+                        }
+                        else -> getWrongMessage(definition)
+                    })
+                }
+            }
+            "exit" -> return println("Bye bye!")
+        }
     }
 }
+
+private fun readInput() = readLine()!!.trim()
 
 private fun getWrongMessage(definition: String, anotherTerm: String? = null): String {
     val message = "Wrong answer. The correct one is \"$definition\""
